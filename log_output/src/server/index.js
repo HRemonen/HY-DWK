361f07fcdaa651e62be1d5ebdec602b3162d935e
Henri Remonen <henri@remonen.fi>
2024-06-09 11:57:31 +0300

    chore: release unpushed changes

--- log_output/src/server/index.js
+++ log_output/src/server/index.js
@@ -4,9 +4,12 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
+const directory = path.join('/', 'usr', 'src', 'app', 'logs')
+const filePath = path.join(directory, 'status.txt')
+
 app.get('/', (req, res) => {
   try {
-    const hash = fs.readFileSync('logs/status.txt', 'utf8')
+    const hash = fs.readFileSync(filePath, 'utf8')
     if (!hash) res.status(404).send('No hash found')
     
     res.send(hash)
