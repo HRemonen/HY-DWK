b8df03a17e68386ac496182196c71559f493dd0f
Henri Remonen <henri@remonen.fi>
2024-06-09 10:48:57 +0300

    refactor(log output): update server logic and generator package name

--- part1/log_output/src/server/index.js
+++ part1/log_output/src/server/index.js
@@ -1,21 +1,18 @@
-import crypto from 'crypto'
+import fs from 'fs'
 import express from 'express'
 
 const app = express()
 const PORT = 3000
 
-const randomUUID = crypto.randomUUID()
-
-const getHashNow = () => {
-  const timeStamp = new Date().toISOString()
-
-  return `${timeStamp}: ${randomUUID}`
-}
-
 app.get('/', (req, res) => {
-  const hash = getHashNow()
-
-  res.send(hash)
+  try {
+    const hash = fs.readFileSync('logs/status.txt', 'utf8')
+    if (!hash) res.status(404).send('No hash found')
+    
+    res.send(hash)
+  } catch (err) {
+    res.status(500).send(err.message)
+  }
 })
 
 app.listen(PORT, () => {
