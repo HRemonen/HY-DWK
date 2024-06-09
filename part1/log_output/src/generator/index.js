e449d0500962d62bc2d55fad8e1fee0c000cd475
Henri Remonen <henri@remonen.fi>
2024-06-09 11:16:30 +0300

    feat(log output): update the logs path

--- part1/log_output/src/generator/index.js
+++ part1/log_output/src/generator/index.js
@@ -1,7 +1,18 @@
 import fs from 'fs'
+import path from 'path'
 import crypto from 'crypto'
 
 const randomUUID = crypto.randomUUID()
+
+const directory = path.join('/', 'usr', 'src', 'app', 'logs')
+const filePath = path.join(directory, 'status.txt')
+
+const fileAlreadyExists = async () => new Promise(res => {
+  fs.stat(filePath, (err, stats) => {
+    if (err || !stats) return res(false)
+    return res(true)
+  })
+})
 
 const getHashNow = () => {
   const timeStamp = new Date().toISOString()
@@ -9,10 +20,17 @@ const getHashNow = () => {
   return `${timeStamp}: ${randomUUID}`
 }
 
-setInterval(() => {
+setInterval(async () => {
+  const fileExists = await fileAlreadyExists()
   const hash = getHashNow()
 
-  fs.writeFile('logs/status.txt', hash, (err) => {
+  if (!fileExists) {
+    fs.mkdir(directory, { recursive: true }, (err) => {
+      if (err) console.error(err)
+    })
+  }
+
+  fs.writeFile(filePath, hash, (err) => {
     if (err) console.error(err)
     console.log(hash)
   }
