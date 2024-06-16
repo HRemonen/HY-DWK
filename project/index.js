bbaaae8772669ce8af2fc570e943d50e16433761
Henri Remonen <henri@remonen.fi>
2024-06-16 11:31:16 +0300

    feat: project ex 2.02

--- project/index.js
+++ project/index.js
@@ -3,6 +3,8 @@ import fs from 'fs'
 import path from 'path'
 import express from 'express'
 import request from 'request'
+import axios from 'axios'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -28,7 +30,7 @@ const downloadImage = async (uri, filename, callback) => {
   })
 }
 
-setInterval(async () => {
+const downloadImageNow = async () => {
   const fileExists = await fileAlreadyExists()
 
   if (!fileExists) {
@@ -40,14 +42,45 @@ setInterval(async () => {
   downloadImage('https://picsum.photos/1200', imgPath, () => {
     console.log('Image downloaded')
   })
+}
+
+downloadImageNow()
+
+setInterval(async () => {
+  await downloadImageNow()
 }, 60 * 60000)
 
+app.use(cors())
+
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('files'))
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'DWK project', message: 'Hello world!' })
+app.get('/', async (req, res) => {
+  const todos = await axios.get('http://dwkproject-backend-svc:2345/todos')
+    .then(response => { 
+      console.log(response)
+
+      return response.data.data
+    })
+    .catch(err => console.error(err))
+  
+  console.log(todos)
+
+  res.render('index', { title: 'DWK project', message: 'Hello world!', todos })
+})
+
+app.post('/', async (req, res) => {
+  const { title } = req.body
+
+  await axios.post('http://dwkproject-backend-svc:2345/todos', { title })
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(err => console.error(err))
+
+  res.redirect('/')
 })
 
 app.listen(PORT, () => {
