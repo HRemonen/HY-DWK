b64935bc6d4170c87dd608a752a55c83d35d930e
Henri Remonen <henri@remonen.fi>
2024-06-18 19:45:34 +0300

    feat: deprecate old generator logic

--- log_output/src/generator/index.js
+++ log_output/src/generator/index.js
@@ -1,38 +1,19 @@
-import fs from 'fs'
-import path from 'path'
 import crypto from 'crypto'
 
 const randomUUID = crypto.randomUUID()
 
-const directory = path.join('/', 'usr', 'src', 'app', 'logs')
-const filePath = path.join(directory, 'status.txt')
+let hash
 
-const fileAlreadyExists = async () => new Promise(res => {
-  fs.stat(filePath, (err, stats) => {
-    if (err || !stats) return res(false)
-    return res(true)
-  })
-})
-
-const getHashNow = () => {
+const generateNewHash = () => {
   const timeStamp = new Date().toISOString()
 
   return `${timeStamp}: ${randomUUID}`
 }
 
+const getHash = () => hash
+
 setInterval(async () => {
-  const fileExists = await fileAlreadyExists()
-  const hash = getHashNow()
+  hash = generateNewHash()
 
-  if (!fileExists) {
-    fs.mkdir(directory, { recursive: true }, (err) => {
-      if (err) console.error(err)
-    })
-  }
-
-  fs.writeFile(filePath, hash, (err) => {
-    if (err) console.error(err)
-    console.log(hash)
-  }
-  )
+  console.log("Generated new hash", getHash())
 }, 5000)
