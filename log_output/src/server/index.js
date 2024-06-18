053ec13d0d9ed5592f8bf84a95c5f5a737a7ee4a
Henri Remonen <henri@remonen.fi>
2024-06-18 20:15:58 +0300

    feat: acually do not remove the log_output pvc

--- log_output/src/server/index.js
+++ log_output/src/server/index.js
@@ -1,17 +1,19 @@
 import fs from 'fs'
+import path from 'path'
 import express from 'express'
-import { getHash } from '../generator/index.js'
 
 const app = express()
 const PORT = 3000
 
+const directory = path.join('/', 'usr', 'src', 'app', 'logs')
+const statusPath = path.join(directory, 'status.txt')
+
 app.get('/', (req, res) => {
   try {
-    const hash = getHash()
-
-    const pingpong = 'pong'
+    const hash = fs.readFileSync(statusPath, 'utf8')
+    if (!hash) res.status(404).send('No hash found')
     
-    res.send(`${hash}: ${pingpong}`)
+    res.send(`${hash}. ${pingpong}`)
   } catch (err) {
     res.status(500).send(err.message)
   }
