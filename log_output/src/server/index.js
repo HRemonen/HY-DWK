de0e31cd5929996d27d6ae9bf0becc6cc9efed1d
Henri Remonen <henri@remonen.fi>
2024-06-24 10:51:00 +0300

    feat: ex 2.06

--- log_output/src/server/index.js
+++ log_output/src/server/index.js
@@ -6,20 +6,29 @@ import axios from 'axios'
 const app = express()
 const PORT = 3000
 
-const directory = path.join('/', 'usr', 'src', 'app', 'logs')
-const statusPath = path.join(directory, 'status.txt')
+const logsDir = path.join('/', 'usr', 'src', 'app', 'logs')
+const configDir = path.join('/', 'usr', 'src', 'app', 'config')
 
+const statusPath = path.join(logsDir, 'status.txt')
+const informationPath = path.join(configDir, 'information.txt')
 
 app.get('/', async (req, res) => {
-  const hash = fs.readFileSync(statusPath, 'utf8')
+  const information = fs.readFileSync(informationPath, 'utf8')
+  if (!information) res.status(404).send('No config information found')
+  res.write(`file content: ${information}` + '\n')
 
+  if (process.env.MESSAGE) res.write(`MESSAGE: ${process.env.MESSAGE}` + '\n') 
+
+  const hash = fs.readFileSync(statusPath, 'utf8')
   if (!hash) res.status(404).send('No hash found')
+
+  res.write(hash + '\n')
 
   const pingpong = await axios.get('http://pingpong-svc:2345/pingpong') .then(response => response.data)
     .catch(err => res.status(500).send(err.message)
   )
   
-  res.send(`${hash}. ${pingpong}`)
+  res.write(pingpong).end()
 })
 
 
