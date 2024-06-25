4ee03aa876ce0176b13f99c1172586ed73f975a4
Henri Remonen <henri@remonen.fi>
2024-06-25 10:14:49 +0300

    feat: ex 2.07

--- ping_pong/index.js
+++ ping_pong/index.js
@@ -1,22 +1,35 @@
 import express from 'express'
+import { connectToDatabase, sequelize } from './database.js'
+import { DataTypes } from 'sequelize'
 
 const app = express()
 const PORT = 3000
-
-let counter = 0
 
 app.get('/', (req, res) => {
   res.send('ping')
 })
 
-app.get('/pingpong', (req, res) => {
-  const counterValue = counter
-  const pingpong = `Ping / Pongs: ${counterValue}`
-  counter += 1
+app.get('/pingpong', async (req, res) => {
+  const Pong = sequelize.define('Pong', {
+		count: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			defaultValue: 0,
+		},
+	})
+
+	await sequelize.sync({ alter: true })
+
+	let [pong, created] = await Pong.findOrCreate({ where: {} })
+
+  const pingpong = `Ping / Pongs: ${pong.count}`
+  pong.increment({ count: 1 })
 
   res.status(200).send(pingpong)
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server started in port ${PORT}`)
+
+  await connectToDatabase()
 })
