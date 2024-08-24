d3156b85fd2836d8cd048c6300e8607d27fc8820
Henri Remonen <henri@remonen.fi>
2024-08-24 17:05:51 +0300

    chore: prettier

--- ping_pong/database.js
+++ ping_pong/database.js
@@ -1,42 +1,42 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize } from "sequelize";
 
-const DB_CONNECTION_RETRY_LIMIT = 10
+const DB_CONNECTION_RETRY_LIMIT = 10;
 
 export const sequelize = new Sequelize(
-  'postgres',
-  'postgres',
+  "postgres",
+  "postgres",
   process.env.POSTGRES_PASSWORD,
   {
-    host: 'postgres-svc',
-    dialect: 'postgres',
+    host: "postgres-svc",
+    dialect: "postgres",
   }
-)
+);
 
 const testConnection = async () => {
-  await sequelize.authenticate()
-}
+  await sequelize.authenticate();
+};
 
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const connectToDatabase = async (attempt = 0) => {
   try {
-    await testConnection()
+    await testConnection();
   } catch (err) {
     if (attempt === DB_CONNECTION_RETRY_LIMIT) {
       console.error(`Connection to database failed after ${attempt} attempts`, {
         error: err.stack,
-      })
+      });
 
-      return process.exit(1)
+      return process.exit(1);
     }
     console.log(
       `Connection to database failed! Attempt ${attempt} of ${DB_CONNECTION_RETRY_LIMIT}`
-    )
-    console.error('Database error: ', err)
-    await sleep(5000)
+    );
+    console.error("Database error: ", err);
+    await sleep(5000);
 
-    return connectToDatabase(attempt + 1)
+    return connectToDatabase(attempt + 1);
   }
 
-  return null
-}
+  return null;
+};
